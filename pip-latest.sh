b049d413349a364a9e52cb7aa3c74c1f9bdead40
John Cardarelli <stressed@desserts.yodel.space>
2018-05-15 16:04:06 -0700

    Added function to check for and install apt dependencies

--- pip-latest.sh
+++ pip-latest.sh
@@ -3,11 +3,33 @@
 # Author: John Cardarelli
 
 PIP_PKG="$1"
+APT_PREREQS=("curl" "jq")
+APT_NOT_INSTALLED=()
+
+function install_apt_prereqs () {
+  for package in "${APT_PREREQS[@]}"; do
+    dpkg-query -l $package
+    if [[ $? -eq 0 ]]; then
+      echo "$package is already installed."
+    else
+      echo "$package not found, adding to install list."
+      # append package to install list
+      APT_NOT_INSTALLED+=$package
+    fi
+  done
+
+  if [[ $APT_NOT_INSTALLED -gt 0 ]]; then
+    echo "Installing prerequisite packages..."
+    sudo apt-get install "${APT_NOT_INSTALLED[@]}"
+  fi
+}
 
 # Exit if $PIP_PKG argument is not specified
 if [[ -z $PIP_PKG ]]; then
   echo "ERROR: Please specify a pip package to find the latest version of."
 else
+  install_apt_prereqs
+
   # Store the output of stderr to a variable, throw away stdout, translate each comma and space to a newline
   PIP_CHECK_CMD="$(pip install ${PIP_PKG}== 2>&1 > /dev/null | tr ', ' '\r\n')"
 
