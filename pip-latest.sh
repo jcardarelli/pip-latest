16375905efdd80ad073c2b48ea3e9371a6f3c340
John Cardarelli <stressed@desserts.yodel.space>
2018-05-21 00:59:30 -0700

    Removed echo statements, script only returns version

--- pip-latest.sh
+++ pip-latest.sh
@@ -3,9 +3,6 @@
 # Author: John Cardarelli
 
 PIP_PKG="$1"
-APT_PREREQS=("curl" "jq")
-APT_INSTALL_CMD="sudo apt-get install"
-APT_NOT_INSTALLED=()
 
 #######################################
 # Install prerequisite packages with apt-get
@@ -17,12 +14,16 @@ APT_NOT_INSTALLED=()
 #   None
 #######################################
 install_apt_prereqs() {
+  local APT_PREREQS=("curl" "jq")
+  local APT_INSTALL_CMD="sudo apt-get install"
+  local APT_NOT_INSTALLED=()
   for PACKAGE in "${APT_PREREQS[@]}"; do
     # check if package is installed by testing response code of dpkg-query
     dpkg-query -l $PACKAGE 2>&1 > /dev/null
 
     if [[ $? -eq 0 ]]; then
-      echo "$PACKAGE is already installed."
+      #echo "$PACKAGE is already installed."
+      :
     else
       echo "$PACKAGE not found, adding to install list."
       # append package to install list
@@ -34,8 +35,22 @@ install_apt_prereqs() {
     echo "Installing prerequisite packages..."
     $APT_INSTALL_CMD "${APT_NOT_INSTALLED[@]}"
   else
-    echo "Prerequisite check passed."
+    :
+    #echo "Prerequisite check passed."
   fi
+}
+
+#######################################
+# Store the output of stderr to a variable, throw away stdout, translate each
+# comma and space to a newline
+#######################################
+stupid_pip_check() {
+  pip install ${PIP_PKG}== 2>&1 > /dev/null | tr ', ' '\r\n' | sed '/[a-z]/d' 
+  \| sort -r | head -n 1
+}
+
+ok_pip_check() {
+  curl -s https://pypi.org/pypi/${1}/json | jq --raw-output '.info.version'
 }
 
 # Exit if $PIP_PKG argument is not specified
@@ -43,11 +58,5 @@ if [[ -z $PIP_PKG ]]; then
   echo "ERROR: Please specify a pip package to find the latest version of."
 else
   install_apt_prereqs
-
-  # Store the output of stderr to a variable, throw away stdout, translate each
-  # comma and space to a newline
-  PIP_CHECK_CMD="$(pip install ${PIP_PKG}== 2>&1 > /dev/null | tr ', ' '\r\n' \ 
-  | sed '/[a-z]/d' | sort -r | head -n 1)"
-
-  echo "$PIP_CHECK_CMD"
+  ok_pip_check "$PIP_PKG"
 fi
